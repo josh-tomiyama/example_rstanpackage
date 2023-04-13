65f38f13dfac2f1b4f21e05c229ccd18446f2d66
Tomiyama, Joshua-Michael K <joshua-michael-tomiyama@uiowa.edu>
2023-04-13 06:02:05 -0500

    example package files

---
+++ R/example-package.R
@@ -0,0 +1,16 @@
+#' The 'example' package.
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#'
+#' @docType package
+#' @name example-package
+#' @aliases example
+#' @useDynLib example, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#'
+#' @references
+#' Stan Development Team (NA). RStan: the R interface to Stan. R package version 2.26.13. https://mc-stan.org
+#'
+NULL
